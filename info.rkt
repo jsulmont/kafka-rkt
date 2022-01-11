fdbbe32f87f59ddb6c2658a877aed8bb6500c616
Jan Sulmont <modality@gmail.com>
2022-01-11 10:27:19 +0100

    (yet) another struct reorg

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
-(define collection "rdkafka")
+(define collection "kafka")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/rdkafka.scrbl" ())))
+(define scribblings '(("scribblings/kafka.scrbl" ())))
 (define pkg-desc "A Racket Kafka client built on top of librdkafka")
 (define version "0.1")
 (define pkg-authors '(jsulmont))
